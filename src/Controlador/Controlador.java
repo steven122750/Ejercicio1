81f572d9dada240a406cd9dd3944af227c8db2be
steven122750 <cardona21507@gmail.com>
2022-10-04 21:31:44 -0500

    holaaa

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -56,6 +56,8 @@ public class Controlador implements IControladorService {
 //git merge nombreRama (combinar ramas)
 //git branch -d nombreRama (eliminar rama)
 
+//datos 222
+
 
     @Override
     public boolean guardarDeuda(int codigo, String aQuienDebo, String fechaDeuda, float cantidad) {
